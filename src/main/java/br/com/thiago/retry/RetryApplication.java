3e1f2f94cd60949e8449b721e62f10af787fd3dc
thiagofranceschini <thiagoconsultor1@gmail>
2018-07-09 16:54:01 -0300

    created Entity cidade ; estado

--- src/main/java/br/com/thiago/retry/RetryApplication.java
+++ src/main/java/br/com/thiago/retry/RetryApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.thiago.retry.model.Categoria;
+import br.com.thiago.retry.model.Cidade;
+import br.com.thiago.retry.model.Estado;
 import br.com.thiago.retry.model.Produto;
 import br.com.thiago.retry.service.repositories.CategoriaRepository;
+import br.com.thiago.retry.service.repositories.CidadeRepository;
+import br.com.thiago.retry.service.repositories.EstadoRepository;
 import br.com.thiago.retry.service.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,7 +23,11 @@ public class RetryApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
-
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RetryApplication.class, args);
 	}
@@ -43,11 +51,29 @@ public class RetryApplication implements CommandLineRunner {
 		p2.getCategorias().add(cat2);
 		p3.getCategorias().add(cat1);
 
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().add(c1);
+		est2.getCidades().add(c2);
+		est2.getCidades().add(c3);
+
 		this.categoriaRepository.save(cat1);
 		this.categoriaRepository.save(cat2);
+		
 		this.produtoRepository.save(p1);
 		this.produtoRepository.save(p2);
 		this.produtoRepository.save(p3);
+		
+		this.estadoRepository.save(est1);
+		this.estadoRepository.save(est2);
+		this.cidadeRepository.save(c1);
+		this.cidadeRepository.save(c2);
+		this.cidadeRepository.save(c3);
 
 	}
 }
