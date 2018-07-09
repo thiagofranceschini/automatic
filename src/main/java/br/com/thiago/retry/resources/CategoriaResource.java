ce985c7c16835d8a265f4607c634cdff4ba27ccb
thiagofranceschini <thiagoconsultor1@gmail>
2018-07-09 11:17:16 -0300

    first test with RestCroller; First Model Categoria

--- src/main/java/br/com/thiago/retry/resources/CategoriaResource.java
+++ src/main/java/br/com/thiago/retry/resources/CategoriaResource.java
@@ -1,16 +1,26 @@
 package br.com.thiago.retry.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.thiago.retry.model.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Listagem em teste do constroller de categorias!";
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Object> listar() {
+		Categoria categoria1 = new Categoria(1, "Informática");
+		Categoria categoria2 = new Categoria(2, "Escritório");
+		List<Object> list = new ArrayList<>();
+		list.add(categoria1);
+		list.add(categoria2);
+		return list;
 	}
-	
+
 }
